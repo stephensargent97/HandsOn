9c98eff63e87e92f383da103085bd73b20270a00
stephensargent97 <stephensargent897@gmail.com>
2021-11-13 19:51:51 -0600

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'list',
+    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  },
+  { path: 'toc', loadChildren: './toc/toc.module#TocPageModule' },
+  { path: 'chapter1', loadChildren: './chapter1/chapter1.module#Chapter1PageModule' }
+  ];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/toc/toc.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toc',
+  templateUrl: './toc.page.html',
+  styleUrls: ['./toc.page.scss'],
+})
+export class TocPage implements OnInit {
+  public chapters = [
+    {
+      title: "Chapter 1 | The Beggining",
+      url: "/chapter1"
+  }];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
